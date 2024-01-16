aad49b63c0e0fed07506820fe3ac807123f36aa6
sophia <sophiadeng@netappstore.net>
2024-01-16 11:53:41 +0800

    modify index

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
+    console.log('db init');
     app.listen(3000, () => console.log('Listening on port 3000'));
 }).catch((err) => {
     console.error(err);
